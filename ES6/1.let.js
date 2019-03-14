1fa40e3defbeb10cbff49ec15fd1ff54ca055db6
panDaxiang <734191973@qq.com>
2019-03-14 23:58:31 +0800

    更改目录

---
+++ ES6/1.let.js
@@ -0,0 +1,27 @@
+{
+  // 不支持变量提升
+  console.log(a); // 此处报错
+
+  let a = "a";
+}
+
+{
+  // 不能重复声明同一个变量
+  let a = "a";
+  let a = "abc"; // 报错
+}
+
+{
+  // 支持块级作用域
+  if (true) {
+    let a = "a";
+  }
+
+  console.log(a); // 报错
+
+  for (let i = 0; i < 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    });
+  }
+}
